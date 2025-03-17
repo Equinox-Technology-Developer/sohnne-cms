d8f10cafd3f6a1965dc11937b6a575fa36d4ed50
Yulius Kevin <yuliuskevin66@gmail.com>
2025-03-17 13:52:18 +0700

    adjust s3 config

--- config/plugins.js
+++ config/plugins.js
@@ -1,26 +1,24 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
-          s3Options: {
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: env('AWS_ACCESS_SECRET'),
-            region: env('AWS_REGION'),
-            params: {
-              Bucket: env('AWS_BUCKET_NAME'),
-            },
-          }
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),  // This matches your env var
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
         },
-        // These parameters could solve issues with ACL public-read access
-        actionOptions: {
-          upload: {
-            ACL: null
-          },
-          uploadStream: {
-            ACL: null
-          },
-        }
       },
-    }
-  });
+      // These parameters could solve issues with ACL public-read access
+      actionOptions: {
+        upload: {
+          ACL: null
+        },
+        uploadStream: {
+          ACL: null
+        },
+      }
+    },
+  }
+});
