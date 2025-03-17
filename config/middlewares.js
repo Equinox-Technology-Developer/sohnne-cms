bd3dc5be1b3ead7ee505c5cbd99782cd7b1caece
Yulius Kevin <yuliuskevin66@gmail.com>
2025-03-17 15:33:11 +0700

    update csp

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,36 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'https://cdn.sohnne.com',  // Add your CDN domain here
+            'https://d1h30a9w33js5b.cloudfront.net'  // Add your CloudFront domain as well
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'https://cdn.sohnne.com',  // Add your CDN domain here
+            'https://d1h30a9w33js5b.cloudfront.net'  // Add your CloudFront domain as well
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
